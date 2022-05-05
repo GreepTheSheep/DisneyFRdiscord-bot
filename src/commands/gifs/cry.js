74f1bbb59e97e3c847daf8e889c9b9e6e70250d4
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-05-05 11:44:02 +0200

    Create cry.js

---
+++ src/commands/gifs/cry.js
@@ -0,0 +1,75 @@
+const Command = require('../../structures/Command'),
+    {MessageEmbed, CommandInteraction, SelectMenuInteraction, Message, MessageActionRow, MessageButton, MessageSelectMenu} = require('discord.js'),
+    gifsObj = require('../../../assets/command-gifs.json');
+
+/**
+ * Set the command here, it's what we'll type in the message
+ * @type {string}
+ */
+exports.name = 'cry';
+
+
+/**
+ * Set the description here, this is what will show up when you need help for the command
+ * @type {string}
+ */
+exports.description = 'Pleurs :\'(';
+
+
+/**
+ * Set the command arguments here, this is what will show up when you type the command
+ * @type {Command.commandArgs[]}
+ */
+exports.args = [
+    {
+        name: 'personne',
+        description: 'A qui rejeter la faute?',
+        type: 'user',
+        required: false
+    }
+];
+
+/**
+ * Set the usage here, this is what will show up when you type the command
+ * This part is executed as slash command
+ * @param {CommandInteraction} interaction
+ * @param {Command[]} commands
+ */
+exports.execute = async (interaction, commands) => {
+    let personne = interaction.options.getUser('personne'),
+        array = gifsObj[this.name],
+        gifUrl = randomItem(array),
+        embed = new MessageEmbed();
+
+    embed.setImage(gifUrl);
+    embed.setColor('DARK_BLUE');
+
+    if (personne) embed.setDescription(`<@${interaction.member.id}> pleure a cause de <@${personne.id}>`);
+    else embed.setDescription(`<@${interaction.member.id}> pleure`);
+
+    interaction.reply({
+        embeds: [embed]
+    });
+};
+
+/**
+ * This method is executed when an a button is clicked in the message
+ * @param {ButtonInteraction} interaction
+ * @param {string} buttonId
+ * @param {string} argument
+ * @param {Command[]} commands
+ */
+exports.executeButton = async (interaction, buttonId, argument, commands) => {};
+
+/**
+ * This method is executed when an update is made in a selectMenu
+ * @param {SelectMenuInteraction} interaction
+ * @param {string} categoryId
+ * @param {string} argument
+ * @param {Command[]} commands
+ */
+exports.executeSelectMenu = async (interaction, categoryId, argument, commands) => {};
+
+function randomItem(array){
+    return array[Math.floor(Math.random() * array.length)];
+}
