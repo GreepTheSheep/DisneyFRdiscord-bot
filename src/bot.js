12b0923627047c77ccc35ec0b8e2026bd8c2e185
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-05-12 15:10:57 +0200

    Revert "Add intents"
    This reverts commit 7c13c74d481ab4be5c38926cf5bd8a02d8766972.

--- src/bot.js
+++ src/bot.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Command = require('./structures/Command'),
     { Client, Intents } = require('discord.js'),
     client = new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INTEGRATIONS]
+        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
     }),
     { readFile, writeFileSync } = require('fs');
 
