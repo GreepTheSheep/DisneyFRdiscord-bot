124e14ca64603ec425a71052551c0882d95e439c
Matthieu Dehon <42576124+GreepTheSheep@users.noreply.github.com>
2022-05-12 13:50:02 +0200

    Update pm2.config.js

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps : [{
-        name: ' DisneyFR-Bot',
+        name: 'DisneyFR-Bot',
         script: 'src/bot.js',
 
         // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
